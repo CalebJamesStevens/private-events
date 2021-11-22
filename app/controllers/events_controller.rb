93eb2c621bee69ef7f2857468d10ea6eb9a4bfa6
Caleb Stevens <calebjamesstevens219@gmail.com>
2021-11-21 22:34:17 -0800

    Adds actions to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,20 +1,29 @@
 class EventsController < ApplicationController
     def index
-
+        @events = Event.all
     end
 
     def show
-    
+        @event = Event.find(params[:event_id])
     end
 
     def new
-
+        @event = Event.new()
     end
 
     def created
-
+        @event = current_user.created_events.build(event_params)
+        if (@event.save)
+            redirect_to @event
+        else
+            render :new
+        end
     end
 
-    
+    private
+
+    def event_params
+        params.require(:event).permit(event_name: params[:event_name], event_location: params[:event_location], event_date: params[:event_date])
+    end
 
 end
