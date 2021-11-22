dca5140df9a21de4337d698319f5ed2fcd8a2d71
Caleb Stevens <calebjamesstevens219@gmail.com>
2021-11-21 19:08:01 -0800

    Creates data base for user and events

--- db/migrate/20211122023227_create_events.rb
+++ db/migrate/20211122023227_create_events.rb
@@ -1,6 +1,11 @@
 class CreateEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :events do |t|
+      t.string :event_name
+      t.string :event_location
+      t.date :event_date
+      t.belongs_to :user
+      
 
       t.timestamps
     end
