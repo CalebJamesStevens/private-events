131717408edf75c426fd17cdf78f5ea79bd46e71
Caleb Stevens <calebjamesstevens219@gmail.com>
2021-11-21 23:29:43 -0800

    Adds event creation functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,7 +13,7 @@ class EventsController < ApplicationController
         @event = Event.new()
     end
 
-    def created
+    def create
         @event = current_user.created_events.build(event_params)
         if (@event.save)
             redirect_to @event
---
+++ db/migrate/20211122072912_add_creator_id_to_events.rb
@@ -0,0 +1,5 @@
+class AddCreatorIdToEvents < ActiveRecord::Migration[6.1]
+  def change
+    
+  end
+end
